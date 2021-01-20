f33e4b3a367c333e6ff177a7b1c56c59b8486ac5
joselondon <copovikingj@gmail.com>
2021-01-20 18:27:06 +0000

    Adds new version of name.rb that takes a first and last name and prints a welome messge to concanted first and last name.

--- variables/name.rb
+++ variables/name.rb
@@ -2,5 +2,12 @@
 
 puts "Please enter your full name"
 name = gets.chomp
-
 puts "Welocome to my program " + name
+puts 
+
+puts "Please enter your first name:"
+first_name = gets.chomp
+puts "Please enter you last name:"
+last_name = gets.chomp
+
+puts "Hello " + first_name + " " + last_name
