ddd8df7535bc482408608a817f08d15f193ec434
joselondon <copovikingj@gmail.com>
2021-01-23 15:44:43 +0000

    Adds answers to question 2

---
+++ methods/exercises/question_2.rb
@@ -0,0 +1,16 @@
+# Question 2 - What do the following expressions evaluate to?
+
+1. x = 2
+# => 2   (returns 2)
+
+2. puts x = 2
+# prints 2 and returns nil
+
+3. p name = "Joe"
+# =>  "Joe"
+
+4. four = "four"
+# => "four"   (returns "four")
+
+5. print something = "nothing"
+# => nil  [print method outputs assigned value and then return nil with no new line]
