7f55b02a705588013032421c7930e7b29b90cc2b
joselondon <copovikingj@gmail.com>
2021-01-23 18:00:58 +0000

    Adds question 6

---
+++ methods/exercises/question_6.rb
@@ -0,0 +1,11 @@
+# What does the following error message tell you?
+
+=begin
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+=end
+
+#  Only one argument was passed to the method which was expecting 2. 
+#  The error occurred at line 1 of the method (as indicated by stack trace)
