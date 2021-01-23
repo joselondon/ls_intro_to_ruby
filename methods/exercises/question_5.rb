5ee2290b7fa94bd06c84e726a5282ecd20a48f67
joselondon <copovikingj@gmail.com>
2021-01-23 17:57:09 +0000

    Adds question-5

---
+++ methods/exercises/question_5.rb
@@ -0,0 +1,12 @@
+# Question 4
+# What will the following code print to the screen?
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+# will now print  "Yippee!!!!"  to screen
+# and will return nil
