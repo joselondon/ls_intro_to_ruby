f5b970f83ef516c0901de78583fff096a2df6680
joselondon <copovikingj@gmail.com>
2021-01-25 19:32:39 +0000

    Adds question_1.rb

--- flow_control/exercises/question_one.rb
+++ flow_control/exercises/question_one.rb
@@ -4,16 +4,17 @@
 # Then type the expressions into irb to see the results.
 
 (32 * 4) >= 129
-# => true
+# => false
 
 false != !true
 # => false
 
 true == 4
-# => true
+# => true WRONG!!   => false 
 
 false == (847 == '874')
 # => true
 
 (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false
 # => true
+
