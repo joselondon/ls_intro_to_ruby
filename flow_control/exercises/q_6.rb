a782f3f0b171cb11a6ebdb45c8e526659418eee3
joselondon <copovikingj@gmail.com>
2021-01-26 19:32:08 +0000

    Adds q_6.rb

---
+++ flow_control/exercises/q_6.rb
@@ -0,0 +1,9 @@
+# q_6.rb
+
+(32 * 4) >= "129" # SHOULD ERROR (STRING TO INTEGER COMPARISON)
+847 == '874' # false
+'847' < '846' # false
+'847' > '846' # true
+'847' > '8478' # false
+'847' < '8478' # true
+
