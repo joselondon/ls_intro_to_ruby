04cd0a95bb1cf6b1b52af98a55687b403d542dfc
joselondon <copovikingj@gmail.com>
2021-01-26 19:26:18 +0000

    Adds q_5 solution

---
+++ flow_control/exercises/q_5.rb
@@ -0,0 +1,14 @@
+# q_5.rb
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+
+equal_to_four(5)
+
+# Fixed error by adding and 'end' to the if/else statement
+
