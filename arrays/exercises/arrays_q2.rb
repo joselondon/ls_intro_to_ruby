2cb4075b8ca49b12681f011977ce089e5d46f274
joselondon <copovikingj@gmail.com>
2021-02-03 19:45:51 +0000

    adds solutions to Q2

---
+++ arrays/exercises/arrays_q2.rb
@@ -0,0 +1,16 @@
+# arrays_q2.rb
+
+# What will the following programs return? What is the value of arr after each?
+
+   arr = ["b", "a"]
+   arr = arr.product(Array(1..3))
+   arr.first.delete(arr.first.last)
+# returns 1
+# arr =  [["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]] 
+# as delete is destructive
+
+2. arr = ["b", "a"]
+   arr = arr.product([Array(1..3)])
+   arr.first.delete(arr.first.last)
+# returns [1, 2, 3]
+# arr = [["b"], ["a", [1, 2, 3]]]
