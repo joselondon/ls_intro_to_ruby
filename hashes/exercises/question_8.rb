3cedb57d9df06427d2d10f22821c8a9ae915ab98
joselondon <copovikingj@gmail.com>
2021-02-12 18:15:56 +0000

    Starts question 8

---
+++ hashes/exercises/question_8.rb
@@ -0,0 +1,21 @@
+# question_8.rb
+
+# Challenge: Given the array...
+
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+  'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+  'flow', 'neon']
+
+# Write a program that prints out groups of words that are anagrams. 
+#Anagrams are words that have the same exact letters in them but in a 
+#different order. Your output should look something like this: 
+
+=begin
+["demo", "dome", "mode"]
+["neon", "none"]
+(etc)
+=end
+
+words.map do |element|
+  element 
+end
