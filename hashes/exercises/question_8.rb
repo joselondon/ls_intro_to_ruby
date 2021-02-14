7607a4832e0f9db0d472ca8d87e1c58abc47f6a2
joselondon <copovikingj@gmail.com>
2021-02-14 16:17:14 +0000

    minor changes to question 8 solution to improve readability

--- hashes/exercises/question_8.rb
+++ hashes/exercises/question_8.rb
@@ -17,23 +17,18 @@ words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
 =end
 
 
-# iterate over array
-# sort each word in the array alphabetically
-# compare each word with that sorted word  (stored as hash key)
-# if key already exists push unsorted word into an array (value of hash key)
-# otherwise create new key with sorted word
-#  when all completed print array values of hash
-
-
 sorted_word_hash = {}
 
 words.each do |word|
+
   sorted_word = word.chars.sort.join
+
   if sorted_word_hash.has_key?(sorted_word) 
     sorted_word_hash[sorted_word] << word
   else
     sorted_word_hash[sorted_word] = [word]
   end
+  
 end
 
 sorted_word_hash.each { |k,v| p v}
