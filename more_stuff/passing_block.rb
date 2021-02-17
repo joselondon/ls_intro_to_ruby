567d97911eb35ba8a5d5aa3205e6bb6d9b569324
joselondon <copovikingj@gmail.com>
2021-02-17 17:48:13 +0000

    Refactored passing_block.rb to make more complex example

--- more_stuff/passing_block.rb
+++ more_stuff/passing_block.rb
@@ -1,9 +1,10 @@
 # passing_block.rb
 
-def take_block(&block)
-  block.call
+def take_block(number, &block)
+  block.call(number)
 end
 
-take_block do
-  puts "Block being called in the method!"
+numb = 42
+take_block(numb) do |num|
+  puts "Block being called in the method! #{num}"
 end
