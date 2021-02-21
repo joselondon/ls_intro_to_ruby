9614fa49b59ccf489753d77a6d2a0e5776899681
joselondon <copovikingj@gmail.com>
2021-02-21 16:44:34 +0000

    Adds solution to exerise 8

---
+++ final_exercises/exercise_8.rb
@@ -0,0 +1,15 @@
+# Create a Hash, with one key-value pair, using both Ruby syntax styles.
+
+# Syntax style 1
+hash_1 = Hash.new
+puts hash_1[1] = "one"
+
+# Syntax style 2
+p hash_2 = {
+  b: "two"
+}
+
+# syntax style 3
+p hash_3  = {
+  3=> "three"
+}
