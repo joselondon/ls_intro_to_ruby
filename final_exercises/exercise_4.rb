34a04f2040b24bccf5e54e72bc067c9847eb6d4e
joselondon <copovikingj@gmail.com>
2021-02-22 19:50:43 +0000

    Tidies up file names

--- final_exercises/exercise_4.rb
+++ final_exercises/exercise_4.rb
@@ -1,5 +1,6 @@
 # Exercise 4.  Append 11 to the end of the original array. 
 # Prepend 0 to the beginning.
 
-p [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].push(11).unshift(0)
-
+p [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].push(11)
+puts "and"
+p [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].insert(0, 0)
