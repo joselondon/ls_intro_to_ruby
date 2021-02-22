9aa5a10570a26ab0d1ab228a5fecfdad408eab4e
joselondon <copovikingj@gmail.com>
2021-02-22 08:49:37 +0000

    Adds solution to exercise 10

---
+++ final_exercises/exercise_10.rb
@@ -0,0 +1,9 @@
+# Can hash values be arrays? Can you have an array of hashes? (give examples)
+
+hash_of_arrays = {
+  a: [1, 2, 3],
+  b: [4, 5, 6],
+  c: [8, 8, 9]
+}
+
+array_of_hashes = [a = {a: 1}, b= {b: 2}]
