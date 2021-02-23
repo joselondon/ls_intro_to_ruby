25aac8d37a3303f7fd4a3d2d7430b0389cfc04b1
joselondon <copovikingj@gmail.com>
2021-02-23 20:14:21 +0000

    Adds solutions to exercises 11, 12 and 13

---
+++ final_exercises/exercise_13.rb
@@ -0,0 +1,14 @@
+# Use Ruby's Array method delete_if and String method start_with? 
+# to delete all of the words that begin with an "s" in the following array.
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if { |word| word.start_with?('s') }
+
+p arr
+
+arr2 = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr2.delete_if { |word| word.start_with?('s', 'w') }
+
+p arr2
